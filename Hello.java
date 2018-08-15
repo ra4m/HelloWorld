84c7a8b969931d6c4cd100207485177f12921a28
Raam Noble <ra4m@outlook.com>
2018-08-14 19:05:55 -0700

    Print HelloWorld 10 times

--- Hello.java
+++ Hello.java
@@ -2,7 +2,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World..!!");
+		for (int i=0; i<10; i++) {
+
+			System.out.println("Hello World..!!");
+
+		}
 
 	}
 
